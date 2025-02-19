477e84906f7ed857eaf2bbf9fd2369a7aa85eca7
Lucas Teixeira <82536301+LucassTeixeiraN@users.noreply.github.com>
2025-02-19 14:44:47 -0300

    Alterou o modal para inferir a prioridade da tarefa. Agora é possível criar novas tasks

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,22 @@ import { useEffect, useState } from "react"
 
 export default function App() {
 
+    type Task = {
+        id: number;
+        text: string;
+        priority: string;
+    }
+
     const [modalHandle, setModalHandle] = useState(false)
     const [newTask, setNewTask] = useState("")
     const [tasksAmount, setTasksAmount] = useState(0)
     const [currentTaskId, setCurrentTaskId] = useState(0)
+    const [tasks, setTasks] = useState<Task[]>([])
+
+
+    const idHandle = () => {
+        setCurrentTaskId(currentTaskId + 1)
+    }
 
     const changeModal = () => {
         if (modalHandle) {
@@ -20,20 +32,22 @@ export default function App() {
         }
     }
 
-    const newTaskHandle = (task: string) => {
-        setNewTask(task)
+    const newTaskHandle = (task: Task) => {
+        setTasks(prevTasks => [...prevTasks, task])
     }
 
-    useEffect(()=>{
-        console.log(newTask)
-    }, [newTask])
+    const task = tasks.map((el) => <Task task = {el}/>)
+
+    useEffect(()=> {
+        task
+    }, [tasks])
 
     return(
         <>
             <div className="tasks">
                 <AddBtn changeModal={changeModal}/>
-                <Task taskId={currentTaskId}/>
-                {modalHandle && <Modal text={"Criar Tarefa"} input={true} btnText="Criar" newTaskHandle={newTaskHandle} changeModal={changeModal}/>}
+                {task}
+                {modalHandle && <Modal text={"Criar Tarefa"} input={true} btnText="Criar" newTaskHandle={newTaskHandle} changeModal={changeModal} currentTaskId={currentTaskId} idHandle= {idHandle}/>}
 
             </div>
         </>
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,7 +1,10 @@
 import { useState } from "react"
 import "./Modal.css"
-
-
+type Task = {
+    id: number;
+    text: string;
+    priority: string;
+}
 
 interface ModalInteface {
     text: string,
@@ -9,15 +12,19 @@ interface ModalInteface {
     btnText: string,
     newTaskHandle: any,
     changeModal: any
+    currentTaskId: number
+    idHandle: any
 }
 
 
-export default function Modal( {text, input, btnText, newTaskHandle, changeModal}: ModalInteface ) {
-    const [task, setTask] = useState("")
+export default function Modal( {text, input, btnText, newTaskHandle, changeModal, currentTaskId, idHandle}: ModalInteface ) {
+    const [taskText, setTaskText] = useState("")
+    const [priority, setPriority] = useState("")
 
     const clickModalHandle = () => {
-        newTaskHandle(task)
+        newTaskHandle({id: currentTaskId, text: taskText, priority: priority})
         changeModal()
+        idHandle()
     }
 
     return(
@@ -26,9 +33,22 @@ export default function Modal( {text, input, btnText, newTaskHandle, changeModal
                 <h2 className="modalText">{text}</h2>
                 {input &&
                 <div className="form__group field">
-                    <input onChange={(e) => setTask(e.target.value)} id="modal-input" type="input" className="form__field" placeholder="Tarefa: " />
-                    <label htmlFor="name" className="form__label">Tarefa: </label>
+                    <div className="text-input">
+                        <input onChange={(e) => setTaskText(e.target.value)} id="modal-input" type="input" className="form__field" placeholder="Tarefa: " />
+                        <label htmlFor="name" className="form__label">Tarefa: </label>
+                    </div>
+                    <div className="priority-input">
+                        <label className="priority-input-label">Prioridade: </label> 
+                        <input onChange={(e) => setPriority(e.target.value)} id="myRange" className="slider" step={1} max={3} min={1} type="range" />
+                        <div className="priority-label">
+                            <p>Baixa</p>
+                            <p>Média</p>
+                            <p>Alta</p>
+                        </div>
+                    </div>
                 </div>
+
+
                 }
                 <button className="modal-btn" onClick={() => clickModalHandle()}>{btnText}</button>
             </div>
